9135483ffce402b1f32da597858f94952006e68a
KyleCole01 <kyle@misfitselectronics.com>
2019-06-07 13:06:31 -0400

    initial

---
+++ src/main/kotlin/com/example/kylecolegdp/KylecolegdpApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kylecolegdp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KylecolegdpApplication
+
+fun main(args: Array<String>) {
+    runApplication<KylecolegdpApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kylecolegdp/KylecolegdpApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.kylecolegdp
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KylecolegdpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
